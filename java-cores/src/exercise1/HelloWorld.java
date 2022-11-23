812dfa7929c6d06e0b0c2295b16b08a5197cd5a1
Dat Nguyen <nguyendat260298@gmail.com>
2022-11-23 10:24:19 +0700

    add java-cores

---
+++ java-cores/src/exercise1/HelloWorld.java
@@ -0,0 +1,10 @@
+package exercise1;
+
+public class HelloWorld {
+	/*
+	 * This is the first Java program. It will show "Hello World" message on the screen
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
