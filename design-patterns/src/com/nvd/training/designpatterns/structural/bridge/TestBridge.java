05e4273464fd50a216fa6c8c1ec1a789b6ee62ce
nguyendat281097 <dat.vannguyen@hitachivantara.com>
2022-12-30 17:00:51 +0700

    update file name

--- design-patterns/src/com/nvd/training/designpatterns/structural/bridge/TestBridge.java
+++ design-patterns/src/com/nvd/training/designpatterns/structural/bridge/TestBridge.java
@@ -1,6 +1,6 @@
 package com.nvd.training.designpatterns.structural.bridge;
 
-public class TestBride {
+public class TestBridge {
 	public static void main(String[] args) {
 		IColor red = new Red();
 		IColor blue = new Blue();
